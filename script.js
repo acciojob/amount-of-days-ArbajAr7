ecef6f0622d928ff8222de7d5596fd3ee308ccb8
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-04-24 12:50:18 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function daysOfAYear(year)
+{
+	return isLeapYear ? 366 : 365;
+}
+
+function isLeapYear (year)
+{
+	return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (year) => {
+  //Write your code here
+	console.log(daysOfAYear(year));
+ 
+});
